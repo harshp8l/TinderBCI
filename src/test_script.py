d3351676f244bb69b8727c6fc012bc0507f3b15f
Nate Browne <natebrowne@outlook.com>
2019-03-17 12:27:00 -0700

    script ready for ML model

--- src/test_script.py
+++ src/test_script.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python2
 
+import numpy as np
 from selenium import webdriver
+from sklearn.externals import joblib
 from selenium.webdriver.common.keys import Keys
 
 def refresh_buttons():
@@ -9,8 +11,22 @@ def refresh_buttons():
   buttons.append(driver.find_element_by_xpath("//button[@aria-label='Like']"))
   return buttons
 
-if __name__ == "__main__":
+def get_user_response(model):
+  pass
 
+def run(model):
+
+  while(True):
+    buttons = refresh_buttons()
+
+    action = get_user_response(model)
+
+    if(action == "l"):
+      buttons[1].click()
+    else:
+      buttons[0].click()
+
+def main():
   driver = webdriver.Chrome()
   driver.get("https://tinder.com")
 
@@ -18,18 +34,16 @@ if __name__ == "__main__":
 
   prompt = raw_input("Are you logged in? (y/n): ")
 
+  model = joblib.load('')
+
   try:
-    while(True):
-      buttons = refresh_buttons()
-
-      action = raw_input("(l)ike or (d)islike?: ")
-
-      if(action == "l"):
-        buttons[1].click()
-      else:
-        buttons[0].click()
-
+    run(model)
   except(KeyboardInterrupt, EOFError):
     print "Exiting..."
   finally:
     driver.close()
+
+# Boilerplate to run main
+if __name__ == '__main__':
+  main()
+
